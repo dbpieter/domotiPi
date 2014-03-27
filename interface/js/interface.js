24264017deec4a8be66bcbef22c12d024160305b
Hans Ott <hansott@hotmail.be>
2014-03-27 01:09:13 +0100

    Fixed some bugs, tested for HTML5 errors and added dialogs (modals) to add, modify a device

--- interface/js/interface.js
+++ interface/js/interface.js
@@ -1,4 +1,4 @@
-function init() {
+function initSwitches() {
   // init all switches
   $('.switch').bootstrapSwitch();
 
@@ -8,21 +8,14 @@ function createChart() {
   //Get context with jQuery - using jQuery's .get() method.
   var ctx = $("#temperature").get(0).getContext("2d");
   var data = {
-  	labels : ["January","February","March","April","May","June","July"],
+  	labels : ["12:00","12:15","12:30","12:45","13:00","13:15","13:30"],
   	datasets : [
   		{
-  			fillColor : "rgba(220,220,220,0.5)",
-  			strokeColor : "rgba(220,220,220,1)",
-  			pointColor : "rgba(220,220,220,1)",
-  			pointStrokeColor : "#fff",
-  			data : [65,59,90,81,56,55,40]
-  		},
-  		{
   			fillColor : "rgba(151,187,205,0.5)",
-  			strokeColor : "rgba(151,187,205,1)",
-  			pointColor : "rgba(151,187,205,1)",
+        strokeColor : "rgba(151,187,205,1)",
+        pointColor : "rgba(151,187,205,1)",
   			pointStrokeColor : "#fff",
-  			data : [28,48,40,19,96,27,100]
+  			data : [17,18,18,16,17,20,21]
   		}
   	]
   }
@@ -30,6 +23,6 @@ function createChart() {
 }
 
 $(document).ready(function() {
-  init();
+  initSwitches();
   createChart();
 });
