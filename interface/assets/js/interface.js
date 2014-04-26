043d31fd0331d5b4fcf82fcf6a1b1e819ded928e
Hans Ott <hansott@hotmail.be>
2014-04-26 14:12:15 +0200

    Interface update

--- interface/assets/js/interface.js
+++ interface/assets/js/interface.js
@@ -1,3 +1,78 @@
+var ipaddress = '10.129.17.17';
+
+function addDevice(id, name, pin) {
+  var html = '<div class="col-md-4">';
+  html += '  <div class="device" data-id="' + id + '" data-pin="' + pin + '">';
+  html += '    <div class="well well-sm">';
+  html += '      <h4 class="title">' + name + '</h4>';
+  html += '      <input class="switch" type="checkbox" /><br>';
+  html += '      <p class="edit">';
+  html += '        <a href="#" data-toggle="modal" data-target="#deviceSettings">Modify</a>';
+  html += '      </p>';
+  html += '    </div>';
+  html += '  </div>';
+  html += '</div>';
+  $('#devices > .row').prepend(html);
+}
+
+function initDevices() {
+  var link = "http://" + ipaddress + ":8080/devices";
+  var jqxhr = $.ajax(link)
+  .done(function(data) {
+    for (var i = 0; i < data.length; i++) {
+      addDevice(data[i].id, data[i].name, data[i].pinnumber);
+    }
+    initSwitches();
+  })
+  .fail(function() {
+    showError('Oops. Could not connect to hardware.');
+  })
+}
+
+function setInitState() {
+  var link = "http://" + ipaddress + ":8080/pins";
+  var devices = $('.device');
+  var jqxhr = $.ajax(link)
+  .done(function(data) {
+    $.each($(devices), function(index) {
+      var pin = $(this).attr('data-pin');
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].pin == pin) {
+          $(this).find('.switch').bootstrapSwitch('state', Boolean(data[i].enabled));
+          break;
+        }
+      }
+    });
+  })
+  .fail(function() {
+      showError('Oops. Could not connect to hardware.');
+  })
+}
+
+function turnPin(pin, state) {
+  var link = "http://" + ipaddress + ":8080/pins/" + pin + "/";
+  if (state) {
+    link += "on";
+  }
+  else {
+    link += "off";
+  }
+  // AJAX CALL
+  var jqxhr = $.ajax(link)
+  .done(function() {
+    console.log('turnPin:' + pin + ' ' + state);
+  })
+  .fail(function() {
+    showError('Could not switch on device on pin ' + pin + '.');
+  })
+}
+
+function turnSwitches(state) {
+  $.each($('.switch'), function(index) {
+      $(this).bootstrapSwitch('state', state);
+  });
+}
+
 function createAutoClosingAlert(selector, delay) {
     var alert = $(selector).alert();
     window.setTimeout(function() {
@@ -12,49 +87,29 @@ function showError(msg) {
 }
 
 function initSwitches() {
-// init all switches
+    // init all switches
     $('.switch').bootstrapSwitch();
-    $('#turn-on-all-devices').on('click', function() {
-        $.each($('.switch'), function(index) {
-            $(this).bootstrapSwitch('state', true);
-        });
-        var link = "http://192.168.0.163:8080/pins/allon";
-        // AJAX CALL
-        var jqxhr = $.ajax(link)
-                .done(function() {
-                    console.log("success");
-                })
-                .fail(function() {
-                    console.log("error");
-                })
-                .always(function() {
-                    console.log("complete");
-                });
-    });
-    // showError('There was a problem. Check logbook!');
-    $('#turn-off-all-devices').on('click', function() {
-        $.each($('.switch'), function(index) {
-            $(this).bootstrapSwitch('state', false);
-            // AJAX CALL
-            var link = "http://192.168.0.163:8080/pins/alloff";
-            // AJAX CALL
-            var jqxhr = $.ajax(link)
-                    .done(function() {
-                        console.log("success");
-                    })
-                    .fail(function() {
-                        console.log("error");
-                    })
-                    .always(function() {
-                        console.log("complete");
-                    });
-        });
-    });
+    setInitState();
+}
+
+function addEventHandlers() {
+  $('#turn-on-all-devices').on('click', function() {
+      turnSwitches(true);
+  });
+
+  $('#turn-off-all-devices').on('click', function() {
+      turnSwitches(false);
+  });
+
+  $('.switch').on('switchChange.bootstrapSwitch', function(event, state) {
+    var pin = $(this).parent().parent().parent().parent().attr('data-pin')[0];
+    turnPin(pin, state);
+  });
 }
 
 
 function createChart() {
-//Get context with jQuery - using jQuery's .get() method.
+    //Get context with jQuery - using jQuery's .get() method.
     var ctx = $("#temperature").get(0).getContext("2d");
     var data = {
         labels: ["12:00", "12:15", "12:30", "12:45", "13:00", "13:15", "13:30"],
@@ -72,6 +127,7 @@ function createChart() {
 }
 
 $(document).ready(function() {
-    initSwitches();
+    initDevices();
+    addEventHandlers();
     createChart();
 });
